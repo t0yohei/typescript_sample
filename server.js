3ab5ef69b0ef02b663ceeea589ea69a20979ca30
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2019-04-16 20:04:43 +0900

    [from now] 2019/04/16 20:04:43
    diff --git a/server.js b/server.js
    index 0370bdd..0d5530f 100644
    --- a/server.js
    +++ b/server.js
    @@ -4,22 +4,20 @@ const filSystem = require('fs');
     const server    = http.createServer();
    
     let message  = '';
    -// テンプレートファイルを扱う.
    -let template = filSystem.readFileSync(__dirname + '/sample.ejs', 'utf-8');
    -
    -let randomNumber = 0;
    
     server.on('request', function(request, response) {
    -    randomNumber = Math.round(Math.random() * 100);
    -
    -    let data = ejs.render(template, {
    -        title: 'Hello Hello',
    -        content: "<h2>アクセスありがとうございます！</h2>",
    -        randomNumber: randomNumber,
    -    });
    +    var pokemon = 'ライチュウ';
    +    function sing(){
    +       //JavaScriptは関数内のどこでもvarの宣言を書ける
    +       //これらの変数は関数のどこで定義しても、先頭で定義したものとして見なされる
    +       //var pokemon;
    +       console.log(pokemon);
    +       var pokemon = 'ピカチュウ';
    +       console.log(pokemon);
    +    }
    +    sing();
    
         response.writeHead(200, {'Content-Type' : 'text/html'});
    -    response.write(data);
         response.end();
     });

--- server.js
+++ server.js
@@ -4,22 +4,20 @@ const filSystem = require('fs');
 const server    = http.createServer();
 
 let message  = '';
-// テンプレートファイルを扱う.
-let template = filSystem.readFileSync(__dirname + '/sample.ejs', 'utf-8');
-
-let randomNumber = 0;
 
 server.on('request', function(request, response) {
-    randomNumber = Math.round(Math.random() * 100);
-
-    let data = ejs.render(template, {
-        title: 'Hello Hello',
-        content: "<h2>アクセスありがとうございます！</h2>",
-        randomNumber: randomNumber,
-    });
+    var pokemon = 'ライチュウ';
+    function sing(){
+       //JavaScriptは関数内のどこでもvarの宣言を書ける
+       //これらの変数は関数のどこで定義しても、先頭で定義したものとして見なされる
+       //var pokemon;
+       console.log(pokemon);
+       var pokemon = 'ピカチュウ';
+       console.log(pokemon);
+    }
+    sing();
 
     response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write(data);
     response.end();
 });
 
